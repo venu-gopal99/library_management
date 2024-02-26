abe3aea92d827a00ceb43104cacce20efbed763a
venugopal <venuvcodewonders@gmail.com>
2024-02-26 19:25:01 +0530

    pull

--- Client2/src/adminPage/Home.js
+++ Client2/src/adminPage/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Sidenavbar from "../admin/Sidenavbar";
+import Navbar from "../admin/Navbar"
 import { Box } from "@mui/material";
 function Home() {
   return (
     <>
+    <Navbar/>
       <Box sx={{ display: "flex" }}>
         <Sidenavbar />
         <Box component="main" sx={{ p: 3 }}>
